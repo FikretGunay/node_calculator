37ee4b01724783e5ec11dd2c7e167e555a531c2d
Fikret Gunay <fikretarslangunay@gmail.com>
2024-08-20 14:10:16 +0200

    cc

--- __tests__/integration/integration.test.js
+++ __tests__/integration/integration.test.js
@@ -1,7 +1,7 @@
 import numberCruncher from "../../src/modules/calculation/numberCruncher";
 
 describe("numberCruncher integration tests", function () {
-  // Functional test
+  
   it("should return 2250 given 45 and 2", function () {
     const calcObject = { numOne: 45, numTwo: 2, operator: "percentage" };
     const result = numberCruncher(calcObject);
