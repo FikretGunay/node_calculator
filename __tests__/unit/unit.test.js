c8b9c70c4736dbd99602d0cc8ce60ca629affefc
Fikret Gunay <fikretarslangunay@gmail.com>
2024-08-20 13:36:05 +0200

    jn

--- __tests__/unit/unit.test.js
+++ __tests__/unit/unit.test.js
@@ -2,13 +2,13 @@ import percentage from "../../src/modules/calculation/util/percentage";
 
 describe("percentage tests", function () {
   // Functional test
-  it("should result in 50 when given 500 and 10", function () {
-    expect(percentage(500, 10)).toBe(50);
+  it("should result in 5000 when given 500 and 10", function () {
+    expect(percentage(500, 10)).toBe(5000);
   });
 
   
 
-  it("should result in 5 when given 50 and 10", function () {
-    expect(percentage(50, 10)).toBe(5);
+  it("should result in 500 when given 50 and 10", function () {
+    expect(percentage(50, 10)).toBe(500);
   });
 });
